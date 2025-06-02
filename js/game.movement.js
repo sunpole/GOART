5d65f6e5748bda2dd9604683b48976416d821829
ANTON MAHOMEDOV <akitsatnafiya@gmail.com>
2025-06-02 11:31:31 +0300

    Update game.movement.js

--- js/game.movement.js
+++ js/game.movement.js
@@ -1,18 +1,33 @@
 // game.movement.js
+
+/**
+ * Перемещение игрока в комнату — с учетом "пробки"
+ */
 function moveTo(idx){
   const npcsHere = npcs.filter(n => n.at === idx).length;
   if (npcsHere + 1 > 3) {
-    showEvent('В комнате уже максимальное количество людей (3). Подожди, пока кто-то выйдет!', [{text: 'OK', action: ()=>{}}]);
+    showEvent(
+      'В комнате уже максимальное количество людей (3). Подожди, пока кто-то выйдет!',
+      [{text: 'OK', action: ()=>{}}]
+    );
     return;
   }
   player.at = idx;
   renderAll();
   checkEvents();
 }
+
+/**
+ * Действие: Сделать цветопробу в Аквариуме
+ */
 function makeProba(){
   player.inventory.push('цветопроба');
-  showEvent('Вы сделали цветопробу!',[{text:'Ок',action:renderAll}]);
+  showEvent('Вы сделали цветопробу!', [{text:'Ок',action:renderAll}]);
 }
+
+/**
+ * Действие: Сделать лакировку в Лаке
+ */
 function makeLak(){
   player.inventory.push('лак');
   showEvent('Лак покрыт!', [{text:'Ок',action:renderAll}]);
