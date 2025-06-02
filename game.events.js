a67433d71fce9f2a5511781e903674089b052cc6
ANTON MAHOMEDOV <akitsatnafiya@gmail.com>
2025-06-02 10:47:07 +0300

    Create game.events.js

---
+++ game.events.js
@@ -0,0 +1,8 @@
+// game.events.js
+function checkEvents(){
+  // ... Как раньше: логика квестов, событий, встреч с NPC ...
+}
+// showEvent, showEventNPC — тоже сюда!
+function showEventNPC(text, opts, npc){/* ... */}
+function showEvent(text, opts){/* ... */}
+window.eventAction = function(idx){/* ... */}
