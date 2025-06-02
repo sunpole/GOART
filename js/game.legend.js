c6a900a6cbb680f6ec3657cdfd3bbd471a703bc7
ANTON MAHOMEDOV <akitsatnafiya@gmail.com>
2025-06-02 11:30:53 +0300

    Update game.legend.js

--- js/game.legend.js
+++ js/game.legend.js
@@ -1,15 +1,21 @@
 // game.legend.js
-function legendOpen(){
+
+// Открыть всплывающее окно легенды
+function legendOpen() {
   let html = "<b>Локации:</b><ul>";
-  ROOMS.forEach(r=>{html+=`<li><b>${r.name}</b> ${r.icon}: <span class='actor-desc'>${r.desc}</span></li>`;});
+  ROOMS.forEach(r => {
+    html += `<li><b>${r.name}</b> ${r.icon}: <span class='actor-desc'>${r.desc}</span></li>`;
+  });
   html += "</ul><b>Персонажи:</b><ul>";
-  NPCS_FULL.forEach(n=>{
-    html+=`<li>${n.icon}<b> ${n.name}</b>: <span class='actor-desc'>${n.desc}</span></li>`;
+  NPCS_FULL.forEach(n => {
+    html += `<li>${n.icon}<b> ${n.name}</b>: <span class='actor-desc'>${n.desc}</span></li>`;
   });
   html += "</ul>";
   document.getElementById("legendCont").innerHTML = html;
-  document.getElementById("descModal").style.display='flex';
+  document.getElementById("descModal").style.display = 'flex';
 }
-function legendClose(){
-  document.getElementById("descModal").style.display='none';
+
+// Закрыть всплывающее окно легенды
+function legendClose() {
+  document.getElementById("descModal").style.display = 'none';
 }
