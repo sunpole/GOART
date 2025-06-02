fa38de433af1196e34fd106816f32de280371338
ANTON MAHOMEDOV <akitsatnafiya@gmail.com>
2025-06-02 10:47:16 +0300

    Create game.legend.js

---
+++ game.legend.js
@@ -0,0 +1,15 @@
+// game.legend.js
+function legendOpen(){
+  let html = "<b>Локации:</b><ul>";
+  ROOMS.forEach(r=>{html+=`<li><b>${r.name}</b> ${r.icon}: <span class='actor-desc'>${r.desc}</span></li>`;});
+  html += "</ul><b>Персонажи:</b><ul>";
+  NPCS_FULL.forEach(n=>{
+    html+=`<li>${n.icon}<b> ${n.name}</b>: <span class='actor-desc'>${n.desc}</span></li>`;
+  });
+  html += "</ul>";
+  document.getElementById("legendCont").innerHTML = html;
+  document.getElementById("descModal").style.display='flex';
+}
+function legendClose(){
+  document.getElementById("descModal").style.display='none';
+}
